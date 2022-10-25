8347017161c8a8ae9c16ad9528342dcb4a27d35c
Upsided <matthewlloydw@gmail.com>
2022-10-25 00:57:49 +0000

    feat: add button disable on timeout and typings

--- index.ts
+++ index.ts
@@ -13,28 +13,65 @@ import {
 export type PaginationData = Array<
     (
         | {
+            /**
+             * Message content
+             */
               content: string;
+              /**
+               * Embeds of the message
+               */
               embeds?: MessageEmbed[];
           }
         | {
+            /**
+             * Embeds of the message
+             */
               embeds: MessageEmbed[];
+              /**
+               * Message content
+               */
               content?: string;
           }
     ) & {
+        /**
+         * Message attachments
+         */
         files?: MessageAttachment[];
     }
 >;
 
 export interface PaginationOptions {
+    /**
+     * If the interaction provided has already been replied to.
+     * @property `value` If it was replied to or not
+     * @property `message` The message object (if replied to)
+     */
     alreadyReplied?:
         | {
               value: true;
               message: Message;
           }
         | { value: false };
+    /**
+     * Time in milliseconds until the button collector times out
+     * @default 1000 * 60 * 15 // 15 minutes
+     */
     timeout?: number;
+    /**
+     * If the buttons should also include quick travel buttons: to start, to end
+     * @default true
+     */
     quickTravel?: boolean;
+    /**
+     * Custom filter for the buttons
+     * @default (button) => button.user.id === interaction.user.id
+     */
     filter?: CollectorFilter<[MessageComponentInteraction<CacheType>]>;
+    /**
+     * The message to send when someone who doesn't pass the filter clickes on a button
+     * @default "This isn't yours"
+     */
+    notYoursMessage?: string;
 }
 
 type Concrete<Type> = {
@@ -50,6 +87,12 @@ function editButtons(index: number, max: number, buttons: MessageButton[]) {
     });
 }
 
+/**
+ * Creates a paginated message
+ * @param interaction The interaction to make the pagination off of
+ * @param data The pagination data
+ * @param options Extra pagination options
+ */
 export async function createPaginator(interaction: Interaction, data: PaginationData, options?: PaginationOptions) {
     if (!interaction.isRepliable()) throw new Error('Interaction has to be repliable!');
 
@@ -58,6 +101,7 @@ export async function createPaginator(interaction: Interaction, data: Pagination
         timeout: 1000 * 60 * 15,
         quickTravel: true,
         filter: (inter) => inter.user.id === interaction.user.id,
+        notYoursMessage: 'This isn\'t yours.'
     };
     const config = Object.assign({}, defaults, options ?? {}) as Concrete<PaginationOptions>;
 
@@ -120,7 +164,11 @@ export async function createPaginator(interaction: Interaction, data: Pagination
 
     const collector = message.createMessageComponentCollector({
         time: config.timeout,
-        filter: (b) => config.filter(b) && b.isButton(),
+        filter: (b) => {
+            const valid = config.filter(b) && b.isButton()
+            if (!valid) b.reply({ content: config.notYoursMessage, ephemeral: true });
+            return valid
+        },
     });
 
     collector.on('collect', async (button) => {
@@ -147,6 +195,14 @@ export async function createPaginator(interaction: Interaction, data: Pagination
             embeds: data[index].embeds,
             files: data[index].files,
             components: [new MessageActionRow().addComponents(editButtons(index, data.length - 1, buttons))],
+        });
+    }).on('end', () => {
+        message.edit({ 
+            components: [
+                new MessageActionRow().addComponents(
+                    editButtons(index, data.length - 1, buttons).map(b => b.setDisabled())
+                )
+            ]
         });
     });
 }
